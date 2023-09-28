edf7ac022b97fc616703005160e5afb962178735
joviabebeto <brigittetuyishimire5@gmail.com>
2023-09-28 14:10:03 +0200

    hello

--- js/toggleyepassword.js
+++ js/toggleyepassword.js
@@ -1,2 +1,9 @@
 const togglepassword=document.querySelector("#togglepassworg");
 const password=document.querySelector("#password")
+
+togglepassword.addEventListener("click",function(e){
+    const type=
+    password.getAttribute("type")==="password" ?"text":"password";
+    password.setAttribute("type",type);
+    this.classlist.toggle("fas fa-eye");
+});
